f4468de4fbb9a12925d7cdf25911be09901b1cb2
Dominik Wolf <spam1@dominikw.de>
2020-08-13 12:38:58 +0200

    login screen
    login screen

---
+++ New Unity Project/Assets/Scenes/Registration.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Registration : MonoBehaviour
+
+{
+    public InputField NameInputField;
+    public InputField PWInputField;
+
+    public Button submitButton;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", NameInputField.text);
+        form.AddField("password", PWInputField.text);
+           
+        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("User created successfully");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("User creation failed. Error#:" + www.text);
+        }
+    }
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (NameInputField.text.Length >= 8 && PWInputField.text.Length >= 8);
+    }
+  
+}
---
+++ New Unity Project/Assets/Scenes/WebTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW request = new WWW("http://localhost/sqlconnect/webtest.php");
+        yield return request;
+        string[] webresults = request.text.Split('\t');
+        Debug.Log(webresults[0]);
+        double webnumer = double.Parse(webresults[1]);
+        webnumer *= 2;
+        Debug.Log(webnumer);
+    }
+}
