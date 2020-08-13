9f7fe173a022f405f410b71c20aeb298c0b4bcca
GitPatr <64302994+GitPatr@users.noreply.github.com>
2020-08-13 16:39:36 +0200

    Clean up and sorting

--- New Unity Project/Assets/Scenes/GlobalVariables.cs
+++ New Unity Project/Assets/Scenes/GlobalVariables.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class DBManager
+public static class GlobalVariables
 {
     public static string username;
-    public static int score;
 
     public static bool LoggedIn { get { return username != null; } }
-    
+
     public static void LogOut()
     {
         username = null;
     }
 }
+
