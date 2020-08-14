09cf66b4b5d94b0a92e428a295cb7c76fd201f39
Dominik Wolf <spam1@dominikw.de>
2020-08-14 13:44:29 +0200

    Registrierung und Login auf öffentlicher Datenbank inkl Übergang ins Spiel

---
+++ New Unity Project/Assets/Scenes/Game/GameTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour
+{
+    // public Text ausgabeZeit;
+
+    private int day = GlobalVariables.day;
+    private int month = 1;
+    private int year = 1;
+    private float timePassed = 0;
+    private float timeCounter;
+
+    private void Update() {
+        timeCounter = Time.time - timePassed;
+
+        if (timeCounter > 5) 
+        {
+            timePassed += timeCounter;
+            day++;
+            
+            GlobalVariables.day = day;
+            Debug.Log(day);
+         
+        }
+        
+    }
+
+    
+}
