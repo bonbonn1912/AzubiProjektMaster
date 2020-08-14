dac7faad915435414d5f792c49be5894367ac210
GitPatr <64302994+GitPatr@users.noreply.github.com>
2020-08-14 15:47:31 +0200

    fix login button + Name Changes to be pretty

--- New Unity Project/Assets/Scenes/Main Menu/Login.cs
+++ New Unity Project/Assets/Scenes/Main Menu/Login.cs
@@ -7,7 +7,7 @@ public class Login : MonoBehaviour
     public InputField NameInputField;
     public InputField PWInputField;
 
-    public Button submitButton;
+    public Button LoginButton;
 
     public void CallLogin()
     {
@@ -21,7 +21,7 @@ public class Login : MonoBehaviour
         form.AddField("password", PWInputField.text);
 
         // WWW www = new WWW("http://localhost/sqlconnection/sqlconnect/login.php", form);
-        WWW www = new WWW("https://dominikw.de/AzubiProjekt/login.php");
+        WWW www = new WWW("https://dominikw.de/AzubiProjekt/login.php", form);
         yield return www;
         if (www.text[0] == '0')
         {
@@ -37,6 +37,6 @@ public class Login : MonoBehaviour
 
     public void VerifyInputs()
     {
-        submitButton.interactable = (NameInputField.text.Length >= 1 && PWInputField.text.Length >= 1);
+        LoginButton.interactable = (NameInputField.text.Length >= 1 && PWInputField.text.Length >= 1);
     }
 }
