f1aa0e3015439c6033e71aeb62574872bee195c6
Cjph3r <64399520+Cjph3r@users.noreply.github.com>
2020-09-17 13:14:55 +0200

    MitarbeiterFenster

---
+++ Skripte/ErtragLesen.php
@@ -0,0 +1,23 @@
+<?php
+
+	$con = mysqli_connect('localhost','root',"",'test');
+	
+	/*if (mysqli_connect_errno())
+	{
+		echo "1: Connection failed"; //error code #1 = connection failed
+		exit();
+	}*/
+
+	$readquery = "SELECT * FROM allgemein";
+
+	$runquery = mysqli_query($con, $readquery);
+	
+	//Ziehen von Daten und liefern in einem "array"
+	$info = mysqli_fetch_assoc($runquery);
+	$anzahl = $info["Mitarbeiter"];
+	$kapital = $info["Kapital"];
+	$stufe = $info["Stufe"];
+
+	//Ausgabe von dem Wert Anzahl
+	echo  $anzahl . '-'. $kapital . '-' . $stufe;
+?>
---
+++ Skripte/MitarbeiterEntlassenLesen.php
@@ -0,0 +1,21 @@
+<?php
+
+
+	//Connection festlegen
+	$con = mysqli_connect('localhost','root',"",'test');
+	
+	//Tabelle festlegen
+	$readquery = "SELECT * FROM allgemein";
+	//Verbindungsaufbau mit vorgegebenen Parametern
+	$runquery = mysqli_query($con, $readquery);
+	
+	//Ziehen von Daten aus der DB und in einem Array ausgeben"
+	$info = mysqli_fetch_assoc($runquery);
+	$anzahl = $info["Mitarbeiter"];
+	$kosten = $info["Kosten"];
+	$Kapital = $info["Kapital"];
+
+	//Ausgabe von dem Wert Anzahl
+	echo  $anzahl . '-' . $kosten . '-' . $Kapital;
+
+?>
