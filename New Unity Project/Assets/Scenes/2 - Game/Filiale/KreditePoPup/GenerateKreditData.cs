309d3443d8c05cd69a6ae846705f446259ffa9a6
Dominik Wolf <spam1@dominikw.de>
2020-09-20 15:42:37 +0200

    Alle Kredite werden aufgelistet

---
+++ New Unity Project/Assets/Scenes/2 - Game/Filiale/KreditePoPup/GenerateKreditData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateKreditData : MonoBehaviour
+{
+    public Color TextColor;
+    public Text Scrolltext;
+    public void ButtonFetch()
+    {
+        StartCoroutine(FetchData());
+
+
+    }
+
+    IEnumerator FetchData()
+    {
+        WWWForm fetchform = new WWWForm();
+        fetchform.AddField("user", GlobalVariables.username);
+        WWW fetch = new WWW("https://dominik.grandpa-kitchen.com/PHP-Skripte/FetchData.php", fetchform);
+        yield return fetch;
+        string[] s = fetch.text.Split('/');
+        string line = fetch.text;
+
+        line = line.Replace("@", System.Environment.NewLine);
+        Scrolltext.text = line;
+        Scrolltext.color = TextColor;
+
+
+
+
+
+        /* for(int i = 0; i < s.Length - 1; i++)
+        {
+            Debug.Log(s[i]);
+        }
+
+        Scrolltext.text = s[2]; */
+    }
+}
