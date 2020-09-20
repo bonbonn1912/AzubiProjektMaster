f3977b72557e103380f45e57ba190ea78f7a4532
Dominik Wolf <spam1@dominikw.de>
2020-09-20 18:17:03 +0200

    KrediteSoriteren

--- New Unity Project/Assets/Scenes/2 - Game/Filiale/KreditePoPup/TabletQuerOeffnen.cs
+++ New Unity Project/Assets/Scenes/2 - Game/Filiale/KreditePoPup/TabletQuerOeffnen.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabletQuerOeffnen : MonoBehaviour
 {
     public GameObject App;
     public GameObject ListAllKredits;
+    public Button Aufsteigend;
+    public Button Absteigend;
 
     public void OpenQuer()
     {
         App.SetActive(false);
         ListAllKredits.SetActive(true);
+        Aufsteigend.interactable = false;
+        Absteigend.interactable = false;
     }
 }
