c993bd66744b38293a933555ec87cce5bdb0b59a
Dominik Wolf <spam1@dominikw.de>
2020-09-24 11:00:27 +0200

    Hitboxen für Gebäude angepasst

---
+++ New Unity Project/Assets/Scenes/2 - Game/DWS/HoverTextfeld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverTextfeld : MonoBehaviour
+{
+
+    public GameObject AktienHandelText;
+    public void TextSichtbar()
+    {
+        if(AktienHandelText != null)
+        {
+            bool isActive = AktienHandelText.activeSelf;
+            AktienHandelText.SetActive(!isActive);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scenes/2 - Game/DWS/OpenDWSPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDWSPopup : MonoBehaviour
+{
+    public GameObject DWSPanel;
+
+
+    public void OpenPanel()
+    {
+        if(DWSPanel != null)
+        {
+            bool isActive = DWSPanel.activeSelf;
+            DWSPanel.SetActive(!isActive);
+        }
+    }
+}
