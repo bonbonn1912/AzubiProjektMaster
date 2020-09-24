47229a5c72dffc88e803b6043f2b786a537399ce
Elena <56828140+sudocats@users.noreply.github.com>
2020-09-24 16:27:05 +0200

    euro animation hinzugefügt

---
+++ New Unity Project/Assets/Scenes/2 - Game/Tägliche Routinen/GehaltAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GehaltAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "particles";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalVariables.day%2 == 0)
+        {
+            Debug.Log("30 days");
+        }
+        {
+
+        }
+    }
+}
