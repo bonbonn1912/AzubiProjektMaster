5005cbaffd264117c3870c50021cf6e7e0ca8207
Dominik Wolf <spam1@dominikw.de>
2020-09-24 18:59:55 +0200

    AktienHandel

--- New Unity Project/Assets/Scenes/2 - Game/DWS/AktienKurseGenerieren.cs
+++ New Unity Project/Assets/Scenes/2 - Game/DWS/AktienKurseGenerieren.cs
@@ -18,6 +18,26 @@ public class AktienKurseGenerieren : MonoBehaviour
 
     public Text DifferenzAktie1;
     public Text Aktie1Prozent1;
+    public Text Aktie1Vorzeichen;
+
+    public Text DifferenzAktie2;
+    public Text Aktie2Prozent1;
+    public Text Aktie2Vorzeichen;
+
+    public Text DifferenzAktie3;
+    public Text Aktie3Prozent1;
+    public Text Aktie3Vorzeichen;
+
+    public Text DifferenzAktie4;
+    public Text Aktie4Prozent1;
+    public Text Aktie4Vorzeichen;
+
+    public Text DifferenzAktie5;
+    public Text Aktie5Prozent1;
+    public Text Aktie5Vorzeichen;
+
+
+
     public Color PossDif;
     public Color NegDiv;
 
@@ -56,19 +76,35 @@ public class AktienKurseGenerieren : MonoBehaviour
          Debug.Log("AktuellerAktienkurs: " + a);
         if(a > baseValueAktie1)
         {
-            decimal differenz = Math.Abs(a - baseValueAktie1);
-            Debug.Log("Positive Differenz der Kurse: "+differenz);
-            DifferenzAktie1.text = Convert.ToString(differenz);
+            double c = a;
+            double d = baseValueAktie1;
+
+            double quo = (a / d)-1;
+            quo = quo * 100;
+            
+           
+            DifferenzAktie1.text = (String.Format("{0:0.00}", quo));
             DifferenzAktie1.color = PossDif;
+
+            Aktie1Vorzeichen.text = "+";
+            Aktie1Vorzeichen.color = PossDif;
+         
             Aktie1Prozent1.color = PossDif;
            
         }
         else
         {
+            double c = a;
+            double d = baseValueAktie1;
+
+            double quo = (a / d) - 1;
+            quo = (quo * 100)*-1;
             decimal differenz = Math.Abs(a - baseValueAktie1);
-            Debug.Log("Negative differenz der Kurse: "+differenz);
-            DifferenzAktie1.text = Convert.ToString(differenz);
+            // Debug.Log("Negative differenz der Kurse: "+differenz);
+            DifferenzAktie1.text = (String.Format("{0:0.00}", quo));
             DifferenzAktie1.color = NegDiv;
+            Aktie1Vorzeichen.text = "-";
+            Aktie1Vorzeichen.color = NegDiv;
             Aktie1Prozent1.color = NegDiv;
 
         }
@@ -95,6 +131,39 @@ public class AktienKurseGenerieren : MonoBehaviour
             float newa = a * 0.75f;
             a = Mathf.RoundToInt(newa);
         }
+        if (a > baseValueAktie2)
+        {
+            double c = a;
+            double d = baseValueAktie2;
+
+            double quo = (a / d) - 1;
+            quo = quo * 100;
+
+
+            DifferenzAktie2.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie2.color = PossDif;
+
+            Aktie2Vorzeichen.text = "+";
+            Aktie2Vorzeichen.color = PossDif;
+
+            Aktie2Prozent1.color = PossDif;
+
+        }
+        else
+        {
+            double c = a;
+            double d = baseValueAktie2;
+
+            double quo = (a / d) - 1;
+            quo = (quo * 100) * -1;
+            
+            DifferenzAktie2.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie2.color = NegDiv;
+            Aktie2Vorzeichen.text = "-";
+            Aktie2Vorzeichen.color = NegDiv;
+            Aktie2Prozent1.color = NegDiv;
+
+        }
         baseValueAktie2 = a;
 
         Aktie2Kurs.text = Convert.ToString(a);
@@ -116,6 +185,39 @@ public class AktienKurseGenerieren : MonoBehaviour
         {
             float newa = a * 0.75f;
             a = Mathf.RoundToInt(newa);
+        }
+        if (a > baseValueAktie3)
+        {
+            double c = a;
+            double d = baseValueAktie3;
+
+            double quo = (a / d) - 1;
+            quo = quo * 100;
+
+
+            DifferenzAktie3.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie3.color = PossDif;
+
+            Aktie3Vorzeichen.text = "+";
+            Aktie3Vorzeichen.color = PossDif;
+
+            Aktie3Prozent1.color = PossDif;
+
+        }
+        else
+        {
+            double c = a;
+            double d = baseValueAktie3;
+
+            double quo = (a / d) - 1;
+            quo = (quo * 100) * -1;
+            
+            DifferenzAktie3.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie3.color = NegDiv;
+            Aktie3Vorzeichen.text = "-";
+            Aktie3Vorzeichen.color = NegDiv;
+            Aktie3Prozent1.color = NegDiv;
+
         }
         baseValueAktie3 = a;
 
@@ -139,6 +241,40 @@ public class AktienKurseGenerieren : MonoBehaviour
             float newa = a * 0.75f;
             a = Mathf.RoundToInt(newa);
         }
+        if (a > baseValueAktie4)
+        {
+            double c = a;
+            double d = baseValueAktie4;
+
+            double quo = (a / d) - 1;
+            quo = quo * 100;
+
+
+            DifferenzAktie4.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie4.color = PossDif;
+
+            Aktie4Vorzeichen.text = "+";
+            Aktie4Vorzeichen.color = PossDif;
+
+            Aktie4Prozent1.color = PossDif;
+
+        }
+        else
+        {
+            double c = a;
+            double d = baseValueAktie4;
+
+            double quo = (a / d) - 1;
+            quo = (quo * 100) * -1;
+            decimal differenz = Math.Abs(a - baseValueAktie4);
+            // Debug.Log("Negative differenz der Kurse: "+differenz);
+            DifferenzAktie4.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie4.color = NegDiv;
+            Aktie4Vorzeichen.text = "-";
+            Aktie4Vorzeichen.color = NegDiv;
+            Aktie4Prozent1.color = NegDiv;
+
+        }
         baseValueAktie4 = a;
 
         Aktie4Kurs.text = Convert.ToString(a);
@@ -160,6 +296,39 @@ public class AktienKurseGenerieren : MonoBehaviour
         {
             float newa = a * 0.75f;
             a = Mathf.RoundToInt(newa);
+        }
+        if (a > baseValueAktie5)
+        {
+            double c = a;
+            double d = baseValueAktie5;
+
+            double quo = (a / d) - 1;
+            quo = quo * 100;
+
+
+            DifferenzAktie5.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie5.color = PossDif;
+
+            Aktie5Vorzeichen.text = "+";
+            Aktie5Vorzeichen.color = PossDif;
+
+            Aktie5Prozent1.color = PossDif;
+
+        }
+        else
+        {
+            double c = a;
+            double d = baseValueAktie5;
+
+            double quo = (a / d) - 1;
+            quo = (quo * 100) * -1;
+            
+            DifferenzAktie5.text = (String.Format("{0:0.00}", quo));
+            DifferenzAktie5.color = NegDiv;
+            Aktie5Vorzeichen.text = "-";
+            Aktie5Vorzeichen.color = NegDiv;
+            Aktie5Prozent1.color = NegDiv;
+
         }
         baseValueAktie5 = a;
 
