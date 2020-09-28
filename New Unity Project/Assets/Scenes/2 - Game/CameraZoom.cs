fdb5c329fe4fe2351baaad55f949ac37f1d27f0b
Cjph3r <64399520+Cjph3r@users.noreply.github.com>
2020-09-28 09:39:23 +0200

    Tutorial 0.1

---
+++ New Unity Project/Assets/Scenes/2 - Game/CameraZoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+
+    public bool ZoomActive;
+    public Vector3[] Target;
+
+    public Camera Cam;
+    public float Speed;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam = Camera.main; 
+    }
+
+    public void LateUpdate()
+    {
+        if(ZoomActive)
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 3, Speed);
+
+        }
+        else
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 5 , Speed);
+        }
+    }
+}
