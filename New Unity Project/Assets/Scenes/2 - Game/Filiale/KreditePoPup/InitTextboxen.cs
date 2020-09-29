4cbdd9ca1e3e13019a0ea57df18ff1a664c7e150
Cjph3r <64399520+Cjph3r@users.noreply.github.com>
2020-09-29 16:30:22 +0200

    Main Szene neuaufgebaut für damit Kamerafahrten möglich sind.

--- New Unity Project/Assets/Scenes/2 - Game/Filiale/KreditePoPup/InitTextboxen.cs
+++ New Unity Project/Assets/Scenes/2 - Game/Filiale/KreditePoPup/InitTextboxen.cs
@@ -10,6 +10,7 @@ public class InitTextboxen : MonoBehaviour
 
     public void initTextboxen()
     {
+        Debug.Log("Random Name " + GlobalVariables.username + " Random Nummer " + 2);
         Berater.text = GlobalVariables.username;
         Kredite.text = "2";
     }
