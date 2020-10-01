740421cdaef3581b12cab40868e8007a63aa7267
Cjph3r <64399520+Cjph3r@users.noreply.github.com>
2020-10-01 12:12:14 +0200

    Tutorial fertig gestellt (ohne Texte)

--- New Unity Project/Assets/Scenes/2 - Game/Skripte/General/CameraZoom.cs
+++ New Unity Project/Assets/Scenes/2 - Game/Skripte/General/CameraZoom.cs
@@ -8,12 +8,21 @@ using UnityEngine.UI;
 
 public class CameraZoom : MonoBehaviour
 {
-    public bool ZoomHR;
+    //public bool ZoomActive;
+    public static bool ZoomActiveT0 = false;
+    public static bool ZoomActiveT1 = false;
+    public static bool ZoomActiveT2 = false;
+    public static bool ZoomActiveT3 = false;
+    public static bool ZoomActiveT4 = false;
+   
+
+
     public Vector3[] Target;
 
     public Camera Cam;
-    public float Speed;
-    public float Zoom;
+    public static float Speed = 0.009f;
+    public static float Zoom  = 80;
+
 
 
     // Start is called before the first frame update
@@ -23,22 +32,64 @@ public class CameraZoom : MonoBehaviour
     }
 
 
-    public void ZoomAufHR()
+
+    //LateUpdate is called when active with every frame update
+    public void LateUpdate()
     {
-        ZoomHR = true;
-    }
-  public void LateUpdate()
-    {
-        if(ZoomHR == true)
+        //Testfunktion für die Kamera
+        /* if (ZoomActive)
+         {
+             Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
+             Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[1], Speed);
+         }
+
+         else
+         {
+             Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 190, Speed);
+             Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[0], Speed);
+
+         }*/
+
+
+        if (ZoomActiveT0 == true)
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[0], Speed);
+        }
+
+
+        if (ZoomActiveT1 == true)
         {
             Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
             Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[1], Speed);
 
         }
-        else
+
+        if (ZoomActiveT2 == true)
         {
-            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 190, Speed);
-            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[0], Speed);
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[2], Speed);
+        }
+
+        if (ZoomActiveT3 == true)
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[3], Speed);
+        }
+
+        if (ZoomActiveT4 == true)
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, Zoom, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[4], Speed);
         }
     }
 }
+//IT Zoom 60
+//DWS Zoom 50
+
+//Standart Cam
+/*else
+{
+    Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 190, Speed);
+    Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[0], Speed);
+}*/
