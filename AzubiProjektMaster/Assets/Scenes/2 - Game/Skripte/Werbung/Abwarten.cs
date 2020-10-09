ed88a2c12f5997f69009626560fe3d19300c0af7
Elena <56828140+sudocats@users.noreply.github.com>
2020-10-09 13:55:07 +0200

    vorläufige icons hinzugefügt

---
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Werbung/Abwarten.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Abwarten : MonoBehaviour
+{
+    int kundenAnzahl;
+    
+    public void AbwartenAusgabe()
+    {
+        StartCoroutine(Execute());
+    }
+
+    IEnumerator Execute()
+    {
+        yield return StartCoroutine(DatenLesen());
+        Warten();
+        StartCoroutine(DatenSchreiben());
+    }
+
+    IEnumerator DatenLesen()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", GlobalVariables.username);
+        
+        //kundenlesen
+        WWW www = new WWW();
+        yield return www;
+        string resultKunden = www.text.Split('-')[0];
+        kundenAnzahl = Convert.ToInt32(resultKunden);
+    }
+
+    IEnumerator DatenSchreiben()
+    {
+        string x = Convert.ToString(kundenAnzahl);
+        WWWForm form = new WWWForm();
+        form.AddField("kunden", x);
+        form.AddField("user", GlobalVariables.username);
+
+        //kundenschreiben
+        WWW www = new WWW();
+        yield return www;
+    }
+
+    public void Warten()
+    {
+        kundenAnzahl = kundenAnzahl + 10;
+    }
+}
