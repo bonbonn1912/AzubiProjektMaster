18451cd3acec84bce083fe409f143e57ffd8d97b
Dominik Wolf <spam1@dominikw.de>
2020-10-15 12:36:13 +0200

    Events (ohne Text) an DB angebunden, Tutorial setzen aktiviert

--- AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Events/GenerateEvent.cs
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Events/GenerateEvent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -45,6 +46,7 @@ public class GenerateEvent : MonoBehaviour
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
                     Auswirkung.GetComponent<Text>().text =  "Dies kostet dich: " + b + "€";
+                    TriggerEvent(b, 0);
                     break;
                 }
             case 2:
@@ -58,6 +60,7 @@ public class GenerateEvent : MonoBehaviour
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
                     Auswirkung.GetComponent<Text>().text = "Dies kostet dich: " + b + "€";
+                    TriggerEvent(b, 0);
                     break;
                 }
             case 3:
@@ -71,6 +74,7 @@ public class GenerateEvent : MonoBehaviour
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
                     Auswirkung.GetComponent<Text>().text = "Dies kostet dich: " + b + "€";
+                    TriggerEvent(b, 0);
                     break;
                 }
 
@@ -83,6 +87,7 @@ public class GenerateEvent : MonoBehaviour
         {
             case 4:
                 {
+                    int b = generateNeutraleAuswirkung();
                     Headline.GetComponent<Text>().text = "Die Bank lädt zur Jahreshauptversammlung ein!";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -90,10 +95,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                   StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 5:
                 {
+                    int b = generateNeutraleAuswirkung();
                     Headline.GetComponent<Text>().text = "Der neue James Bond wird in deiner Stadt gedreht!";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -101,10 +109,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                    StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 6:
                 {
+                    int b = generateNeutraleAuswirkung();
                     Headline.GetComponent<Text>().text = "Deine Stadt hat die zufriedensten Einwohner im Land";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -112,10 +123,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                     StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 7:
                 {
+                    int b = generateNeutraleAuswirkung();
                     Headline.GetComponent<Text>().text = "Ein Bundesligist ist zum Trainingslager in deiner Stadt";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -123,6 +137,8 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                     StartCoroutine(Execute(b, 1));
                     break;
                 }
 
@@ -135,6 +151,7 @@ public class GenerateEvent : MonoBehaviour
         {
             case 8:
                 {
+                    int b = generatePositiveAuswirkung();
                     Headline.GetComponent<Text>().text = "Massive Kursgewinne für die Aktien der Bank";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -142,10 +159,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                     StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 9:
                 {
+                    int b = generatePositiveAuswirkung();
                     Headline.GetComponent<Text>().text = "Höchste Anzahl an Bewerbungen seit 20 Jahren!";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -153,10 +173,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                     StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 10:
                 {
+                    int b = generatePositiveAuswirkung();
                     Headline.GetComponent<Text>().text = "Alle Aktien der Finanzbranche steigen";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -164,10 +187,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                   StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 11:
                 {
+                    int b = generatePositiveAuswirkung();
                     Headline.GetComponent<Text>().text = "Deine Bank verkündet die Zusammenarbeit mit einem Tech-Giganten";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -175,10 +201,13 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                     StartCoroutine(Execute(b, 1));
                     break;
                 }
             case 12:
                 {
+                    int b = generatePositiveAuswirkung();
                     Headline.GetComponent<Text>().text = "Neuer Vorstand gewählt";
                     Body.GetComponent<Text>().text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
                         "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " +
@@ -186,6 +215,8 @@ public class GenerateEvent : MonoBehaviour
                         "est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
                         "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
                         "dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                    Auswirkung.GetComponent<Text>().text = "Dies bringt dir: " + b + "€";
+                  StartCoroutine(Execute(b, 1));
                     break;
                 }
 
@@ -193,9 +224,69 @@ public class GenerateEvent : MonoBehaviour
     }
     public int generateNegativeAuswirkung()
     {
-        int b = Random.Range(10000, 80000);
+        int b = Random.Range(GlobalVariables.balance/10, GlobalVariables.balance/2);
         return b;
     }
+
+    public int generateNeutraleAuswirkung()
+    {
+        int b = Random.Range(GlobalVariables.balance / 20, GlobalVariables.balance / 10);
+        return b;
+    }
+    public int generatePositiveAuswirkung()
+    {
+        int b = Random.Range(GlobalVariables.balance / 5, GlobalVariables.balance / 2);
+        return b;
+    }
+
+    public void TriggerEvent(int value, int vorzeichen)
+    {
+        Debug.Log("in trgger");
+        StartCoroutine(Execute(value, vorzeichen));
+    }
+
+    IEnumerator Execute(int value, int vorzeichen)
+    {
+        Debug.Log("in coroutine");
+         yield return new WaitForSeconds(1.5f);
+        switch (vorzeichen)
+        {
+            case 0:
+                {
+                    Debug.Log("Alte Balance" + GlobalVariables.balance);
+                    GlobalVariables.balance = GlobalVariables.balance - value;
+                    Debug.Log("Neue Balance" + GlobalVariables.balance);
+                    WWWForm BalanceUpdate = new WWWForm();
+                    BalanceUpdate.AddField(GlobalVariables.username, "Username");
+                    BalanceUpdate.AddField(Convert.ToString(GlobalVariables.balance), "Balance");
+
+
+                    WWW www = new WWW("https://dominikw.de/AzubiProjekt/UpdateBalance.php", BalanceUpdate);
+                    yield return www;
+                    Debug.Log(www.text);
+                    Debug.Log("Event Kapital updated");
+                    break;
+                }
+            case 1:
+                {
+                    Debug.Log("Alte Balance" + GlobalVariables.balance);
+                    GlobalVariables.balance = GlobalVariables.balance + value;
+                    Debug.Log("Neue Balance" + GlobalVariables.balance);
+                    WWWForm BalanceUpdate = new WWWForm();
+                    BalanceUpdate.AddField( "Username", GlobalVariables.username);
+                    BalanceUpdate.AddField("Balance", Convert.ToString(GlobalVariables.balance));
+
+
+                    WWW www = new WWW("https://dominikw.de/AzubiProjekt/UpdateBalance.php", BalanceUpdate);
+                    yield return www;
+                    Debug.Log(www.text);
+                    Debug.Log("Event Kapital updated");
+                    break;
+                }
+        }
+        
+    }
+
 
 
 
