76688da971af74069f090935287556e45772c0fc
Justin Edelmann <63841288+Jusses3101@users.noreply.github.com>
2020-10-16 11:34:56 +0200

    SpriteAsset für Coins hinzugefügt und Tablet bearbeitet

---
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Achievements/AchStatusCall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AchStatusCall : MonoBehaviour
+{
+    public TextMeshProUGUI Ach1;
+    public TextMeshProUGUI Ach2;
+    public TextMeshProUGUI Ach3;
+    public TextMeshProUGUI Ach4;
+    public TextMeshProUGUI Ach5;
+    public TextMeshProUGUI Ach6;
+    public TextMeshProUGUI Ach7;
+    public TextMeshProUGUI Ach8;
+    public TextMeshProUGUI Ach9;
+
+    void Update()
+    {
+        
+    }
+}
