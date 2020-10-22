d6f86b248e0baffab97b226742cefbc92ad36a68
Dominik Wolf <spam1@dominikw.de>
2020-10-22 09:36:29 +0200

    freitagabend änderung

---
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Werbung/KundenErhoehen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using UnityEngine;
+
+public class KundenErhoehen : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void KundenDB()
+    {
+        StartCoroutine(PushDB());
+    }
+
+    IEnumerator PushDB()
+    {
+        WWWForm pushdb = new WWWForm();
+        int kundenzhal = 25;
+        pushdb.AddField("username", GlobalVariables.username);
+        // pushdb.AddField("kunde", Convert.ToString(kundenzhal));
+       // pushdb.AddField("Balance", GlobalVariables.balance);
+
+        WWW www = new WWW("https://dominikw.de/AzubiProjekt/IncreaseKunde.php", pushdb);
+        yield return www;
+        Debug.Log("kunden erhohene" + www.text);
+    }
+}
