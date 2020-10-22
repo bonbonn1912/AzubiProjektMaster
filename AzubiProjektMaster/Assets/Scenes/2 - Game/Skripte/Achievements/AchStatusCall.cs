e70d88130ab0b2cb5a1ad61875274c6291631d1e
Justin Edelmann <edelmann467@gmail.com>
2020-10-22 12:54:50 +0200

    Included DB Query for one Achievement and changed structure of AchStatusCall.cs Script

--- AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Achievements/AchStatusCall.cs
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/Achievements/AchStatusCall.cs
@@ -1,5 +1,5 @@
 ﻿// Author: Justin Edelmann
-// Co-Author: Colin Schmoll
+// Co-Author: Colin Schmoll & Thorge Siemens
 // Script um die Achievements aus der Datenbank zu lesen und gemäß Datenbankabfrage die Anzahl der erreichten
 // Achievements auf der GUI für den jeweiligen User freizuschalten
 
@@ -7,8 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
-// Datenbank: 0 = 0 Achievements der Kategorie, 1 = 1 Achievement der Kategorie usw.
+using System;
 public class AchStatusCall : MonoBehaviour
 {
     public TextMeshProUGUI Ach1;
@@ -34,96 +33,164 @@ public class AchStatusCall : MonoBehaviour
         Ach7.text = "<sprite=1> <sprite=1> <sprite=1> <sprite=1> <sprite=1> ";
         Ach8.text = "<sprite=1> <sprite=1> <sprite=1> <sprite=1> <sprite=1> ";
         Ach9.text = "<sprite=1> <sprite=1> <sprite=1> <sprite=1> <sprite=1> ";
-        //InvokeRepeating("AchUpdate1", 5.0f, 5.0f); //Dadurch wird Methode alle 5 Sekunden ausgeführt
-        InvokeRepeating("AchUpdate2", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate3", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate4", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate5", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate6", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate7", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate8", 5.0f, 5.0f);
-        //InvokeRepeating("AchUpdate9", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate1", 5.0f, 5.0f); 
+        InvokeRepeating("AchUpdateIT", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate3", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate4", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate5", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate6", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate7", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate8", 5.0f, 5.0f);
+        InvokeRepeating("AchUpdate9", 5.0f, 5.0f);
+    }
+    /* Ach1 =
+     * Ach2 =
+     * Ach3 = 
+     * Ach4 = 
+     * Ach5 =
+     * Ach6 =
+     * Ach7 =
+     * Ach8 =
+     * Ach9 =
+     */
+
+
+
+    //Achievement "absolviere das Tutorial": Anfang
+
+    //Achievement "absolviere das Tutorial":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Erreiche ein maximal Kapital von": Anfang
+
+    //Achievement "Erreiche ein maximal Kapital von":  Ende
+    //-------------------------------------
+
+    //Achievement "Stelle x Mitarbeiter ein  ": Anfang
+
+    //Achievement "Stelle x Mitarbeiter ein  ":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Anzahl x Kunden": Anfang
+
+    //Achievement "Anzahl x Kunden":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Kaufe x Filialen": Anfang
+
+    //Achievement "Kaufe x Filialen":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Vergebe x Kredite": Anfang
+
+    //Achievement "Vergebe x Kredite":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Entlasse keine Mitarbeiter für x Tage": Anfang
+
+    //Achievement "Entlasse keine Mitarbeiter für x Tage":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Verdiene x mit Aktienhandel": Anfang
+
+    //Achievement "Verdiene x mit Aktienhandel":  Ende
+    //-------------------------------------
+
+
+    //Achievement "Upragade das IT Gebäude 5 mal":  Anfang
+    int achievement1;
+    public void AchUpdateIT()
+    {
+        StartCoroutine(ExecuteAchIT());
     }
 
-    void Datenbankabfrage(){
-        
+    IEnumerator ExecuteAchIT()
+    {
+        yield return StartCoroutine(DatenLesenIT());
+        AusgabeIT();
     }
 
-    /*void AchUpdate1()
+    IEnumerator DatenLesenIT()
     {
-
-        //TBD: Datenbankabfrage der Achievements
-        if( WERT DATENBANK = 0)
-        {
-            Ach1.text = "<sprite=1>";
-        }
-        else
-        {
-            Ach1.text = "<sprite=0>";
-        }
-    }*/
-
-    void AchUpdate2()
+        WWWForm form = new WWWForm();
+        form.AddField("user", GlobalVariables.username);
+        WWW www = new WWW("http://localhost/Test/AchievementItLesen.php", form);
+        yield return www;
+        string achievement1Db = www.text.Split('-')[0];
+        achievement1 = Convert.ToInt32(achievement1Db);   
+    }
+    public void AusgabeIT()
     {
-        int test = 4;
-        if (test == 0)
+        if (achievement1 == 0)
         {
             Ach2.text = "<sprite=1> <sprite=1> <sprite=1> <sprite=1> <sprite=1>";
         }
-        else if(test == 1)
+        else if (achievement1 == 1)
         {
             Ach2.text = "<sprite=0> <sprite=1> <sprite=1> <sprite=1> <sprite=1>";
-        } 
-        else if (test == 2)
+        }
+        else if (achievement1 == 2)
         {
             Ach2.text = "<sprite=0> <sprite=0> <sprite=1> <sprite=1> <sprite=1>";
-        } 
-        else if (test == 3)
+        }
+        else if (achievement1 == 3)
         {
             Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=1> <sprite=1>";
         }
-        else if (test == 4)
+        else if (achievement1 == 4)
         {
             Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=0> <sprite=1>";
         }
-        else if (test == 5)
+        else if (achievement1 == 5)
         {
             Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=0> <sprite=0>";
         }
     }
+    //Achievement "Upragade das IT Gebäude 5 mal":  Ende
+    //-------------------------------------
+   
 
-    /*void AchUpdate3()
-    {
 
-    }
 
-    void AchUpdate4()
-    {
 
-    }
 
-    void AchUpdate5()
-    {
-
-    }
-
-    void AchUpdate6()
-    {
-
-    }
-
-    void AchUpdate7()
-    {
-
-    }
-
-    void AchUpdate8()
-    {
-
-    }
-
-    void AchUpdate9()
-    {
-
-    }*/
 }
+
+
+
+
+/*void AchUpdate2()
+{
+    int test = 4;
+    if (test == 0)
+    {
+        Ach2.text = "<sprite=1> <sprite=1> <sprite=1> <sprite=1> <sprite=1>";
+    }
+    else if(test == 1)
+    {
+        Ach2.text = "<sprite=0> <sprite=1> <sprite=1> <sprite=1> <sprite=1>";
+    } 
+    else if (test == 2)
+    {
+        Ach2.text = "<sprite=0> <sprite=0> <sprite=1> <sprite=1> <sprite=1>";
+    } 
+    else if (test == 3)
+    {
+        Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=1> <sprite=1>";
+    }
+    else if (test == 4)
+    {
+        Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=0> <sprite=1>";
+    }
+    else if (test == 5)
+    {
+        Ach2.text = "<sprite=0> <sprite=0> <sprite=0> <sprite=0> <sprite=0>";
+    }
+}*/
+
