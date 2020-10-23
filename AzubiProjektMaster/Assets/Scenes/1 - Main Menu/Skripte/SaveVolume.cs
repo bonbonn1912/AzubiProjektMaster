a80e5dfe71cd01c628ea60dc57e44370bb386f80
MrPaddy21 <p-pajska21@gmail.com>
2020-10-23 10:20:08 +0200

    Volume save now

---
+++ AzubiProjektMaster/Assets/Scenes/1 - Main Menu/Skripte/SaveVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SaveVolume : MonoBehaviour
+{
+    public Slider MVolume;
+    public Scrollbar CashSound;
+
+    // Start is called before the first frame update
+    void Start(){
+        MVolume.value = PlayerPrefs.GetFloat("musikVolume");
+        CashSound.value = PlayerPrefs.GetFloat("cashSound");
+    }
+
+    void Update(){
+        PlayerPrefs.SetFloat("musikVolume", MVolume.value);
+        PlayerPrefs.SetFloat("cashSound", CashSound.value);
+
+    }
+}
