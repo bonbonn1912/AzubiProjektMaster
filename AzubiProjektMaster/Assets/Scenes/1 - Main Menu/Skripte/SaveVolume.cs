cfbc310f6d8fdd90f020bb97308ce8ce44356e61
MrPaddy21 <p-pajska21@gmail.com>
2020-10-29 12:15:02 +0100

    Setting with Screensize

--- AzubiProjektMaster/Assets/Scenes/1 - Main Menu/Skripte/SaveVolume.cs
+++ AzubiProjektMaster/Assets/Scenes/1 - Main Menu/Skripte/SaveVolume.cs
@@ -8,19 +8,16 @@ public class SaveVolume : MonoBehaviour
 {
     public Slider MVolume;
     public Scrollbar CashSound;
-    public Scrollbar ClickSound;
 
     // Start is called before the first frame update
     void Start(){
         MVolume.value = PlayerPrefs.GetFloat("musikVolume");
         CashSound.value = PlayerPrefs.GetFloat("cashSound");
-        ClickSound.value = PlayerPrefs.GetFloat("clickSound");
     }
 
     void Update(){
         PlayerPrefs.SetFloat("musikVolume", MVolume.value);
         PlayerPrefs.SetFloat("cashSound", CashSound.value);
-        PlayerPrefs.SetFloat("clickSound", ClickSound.value);
     }
 
 }
