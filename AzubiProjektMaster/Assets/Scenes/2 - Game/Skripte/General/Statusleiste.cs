3256a3af707672dc627b3358efed3123fe9f2e2c
Dominik Wolf <spam1@dominikw.de>
2020-10-30 08:34:27 +0100

    Minimalechanges

--- AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/General/Statusleiste.cs
+++ AzubiProjektMaster/Assets/Scenes/2 - Game/Skripte/General/Statusleiste.cs
@@ -8,10 +8,13 @@ public class Statusleiste : MonoBehaviour
     //Statusleiste
     public Text textFieldBalance;
     public Text textFieldDay;
+    public Text textFieldKunde;
 
     void Update()
     {
         textFieldBalance.text = GlobalVariables.balance.ToString();
         textFieldDay.text = GlobalVariables.day.ToString();
+        textFieldKunde.text = GlobalVariables.kundenanzahl.ToString();
+
     }
 }
